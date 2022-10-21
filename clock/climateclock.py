56e0fabbeccba9f2b16083a46606c1f054d32394
eco-mathew <60383033+eco-mathew@users.noreply.github.com>
2022-10-21 16:01:06 +0900

    Update climateclock.py
    add current clock display with button_callback

--- clock/climateclock.py
+++ clock/climateclock.py
@@ -11,14 +11,15 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 # Pulled from dateutil without all the dependencies
 from relativedelta import relativedelta
 
+import RPi.GPIO as g
 
 SECONDS_PER_YEAR = 365.25 * 24 * 3600
 
 # Snapshot of API data (the Good Stuff™)
-CARBON_DEADLINE_1 = datetime.fromisoformat("2029-07-22T16:00:03+00:00")
+CARBON_DEADLINE_1 = datetime.fromisoformat("2028-01-01T12:00:00+00:00")
 RENEWABLES_1 = {
     "initial": 11.4,
-    "timestamp": datetime.fromisoformat("2022-11-05T00:00:00+00:00"),
+    "timestamp": datetime.fromisoformat("2022-01-01T00:00:00+00:00"),
     "rate": 2.0428359571070087e-08,
 }
 
@@ -28,6 +29,7 @@ hex2color = lambda x: graphics.Color(
     int(x[-6:-4], 16), int(x[-4:-2], 16), int(x[-2:], 16)
 )
 
+clock_display = False
 
 def carbon_deadline_1():
     return relativedelta(CARBON_DEADLINE_1, datetime.now(timezone.utc))
@@ -37,15 +39,26 @@ def renewables_1():
     t = (datetime.now(timezone.utc) - RENEWABLES_1["timestamp"]).total_seconds()
     return RENEWABLES_1["rate"] * t + RENEWABLES_1["initial"]
 
+def button_callback(channel):
+    global clock_display
+    clock_display = not clock_display
+
 
 def run(options):
+    #GPIO setting
+    g.setwarnings(False)
+    g.setmode(g.BCM)
+    g.setup(25, g.IN, pull_up_down=g.PUD_DOWN)
+
+    g.add_event_detect(25, g.RISING, callback=button_callback, bouncetime=200)
+
     matrix = RGBMatrix(options=options)
     canvas = matrix.CreateFrameCanvas()
 
     f1 = graphics.Font()
     f1.LoadFont(relpath("6x13B.bdf"))
     f2 = graphics.Font()
-    f2.LoadFont(relpath("6x13.bdf"))
+    f2.LoadFont(relpath("5x8.bdf"))
     f3 = graphics.Font()
     f3.LoadFont(relpath("8x13B.bdf"))
     L1 = 13
@@ -60,40 +73,71 @@ def run(options):
     while not time.sleep(0.05):
         canvas.Clear()
 
-        # Deadline
-        now = datetime.now(timezone.utc)
+        if clock_display == False:
+            # Deadline
+            now = datetime.now(timezone.utc)
 
-        # Use relativedelta for leap-year awareness
-        deadline_delta = relativedelta(CARBON_DEADLINE_1, now)
-        years = deadline_delta.years
-        # Extract concrete days from the months & days provided by relativedelta
-        # @rubberduck: 1. Create a relativedelta object rdays containing Δ months & days
-        #              2. Create a concrete time object rdays in the future
-        #              3. Create a timedelta object representing that value - now
-        #              4. Extract its days
-        rdays = relativedelta(months=deadline_delta.months, days=deadline_delta.days)
-        days = ((rdays + now) - now).days
-        hours = deadline_delta.hours
-        minutes = deadline_delta.minutes
-        seconds = deadline_delta.seconds
-        cs = deadline_delta.microseconds // 10000
+            # Use relativedelta for leap-year awareness
+            deadline_delta = relativedelta(CARBON_DEADLINE_1, now)
+            years = deadline_delta.years
+            # Extract concrete days from the months & days provided by relativedelta
+            # @rubberduck: 1. Create a relativedelta object rdays containing Δ months & days
+            #              2. Create a concrete time object rdays in the future
+            #              3. Create a timedelta object representing that value - now
+            #              4. Extract its days
+            rdays = relativedelta(months=deadline_delta.months, days=deadline_delta.days)
+            days = ((rdays + now) - now).days
+            hours = deadline_delta.hours
+            minutes = deadline_delta.minutes
+            seconds = deadline_delta.seconds
+            cs = deadline_delta.microseconds // 10000
 
-        deadline = [
-            [f1, yellow, 1, f"{years:1.0f}"],
-            [f1, alt_yellow, 1, "YEAR " if years == 1 else "YRS"],
-            [f1, yellow, 1, f"{days:03.0f}"],
-            [f1, alt_yellow, 1, "DAY " if days == 1 else "DAYS"],
-        ]
+            deadline = [
+                [f1, yellow, 1, f"{years:1.0f}"],
+                [f2, alt_yellow, 1, "YEAR " if years == 1 else "YRS"],
+                [f1, yellow, 1, f"{days:03.0f}"],
+                [f2, alt_yellow, 1, "DAY " if days == 1 else "DAYS"],
+            ]
 
-        # Lifeline
-        r1 = renewables_1()
-        lifeline = [
-            [f1, yellow, 0, f"{hours:02.0f}"],
-            [f1, yellow, 0, (":", " ")[cs < 50]],
-            [f1, yellow, 0, f"{minutes:02.0f}"],
-            [f1, yellow, 0, (":", " ")[cs < 50]],
-            [f1, yellow, 0, f"{seconds:02.0f}"],
-        ]
+            # Lifeline
+            r1 = renewables_1()
+            lifeline = [
+                [f1, yellow, 0, f"{hours:02.0f}"],
+                [f1, alt_yellow, 0, (":", " ")[cs < 50]],
+                [f1, yellow, 0, f"{minutes:02.0f}"],
+                [f1, alt_yellow, 0, (":", " ")[cs < 50]],
+                [f1, yellow, 0, f"{seconds:02.0f}"],
+            ]
+
+        elif clock_display == True:
+            now = datetime.now(timezone.utc)
+
+            #display current time
+            current_year = now.year
+            current_month = now.month
+            current_day = now.day
+            current_hour = now.hour
+            current_minute = now.minute
+            current_second = now.second
+            current_cs = now.microsecond // 10000
+
+            deadline = [
+                [f1, yellow, 1, f"{current_year}"],
+                [f1, alt_yellow, 1, ". "],
+                [f1, yellow, 1, f"{current_month}"],
+                [f1, alt_yellow, 1, ". "],
+                [f1, yellow, 1, f"{current_day}"],
+                [f1, alt_yellow, 1, ". "],
+            ]
+
+            # Lifeline
+            lifeline = [
+                [f3, yellow, 0, f"{current_hour}"],
+                [f3, alt_yellow, 0, (":", " ")[current_cs < 50]],
+                [f3, yellow, 0, f"{current_minute}"],
+                [f3, alt_yellow, 0, (":", " ")[current_cs < 50]],
+                [f3, yellow, 0, f"{current_second}"],
+            ]            
 
         x = 1
         for font, color, space, string in deadline:
